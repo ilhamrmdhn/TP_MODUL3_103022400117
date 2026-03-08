808bacf3c06b80b70ddc9fe2a188ae91f8e1af80
ilhamrmdhn <ilham712svr@gmail.com>
2026-03-09 01:34:30 +0700

    Menambahkan implementasi kalkulator

--- TP_MODUL3_103022400117/Form1.Designer.cs
+++ TP_MODUL3_103022400117/Form1.Designer.cs
@@ -28,12 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            labelOutput = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            buttonPlus = new Button();
+            button0 = new Button();
+            buttonEqual = new Button();
+            SuspendLayout();
+            // 
+            // labelOutput
+            // 
+            labelOutput.Font = new Font("Segoe UI", 12F);
+            labelOutput.Location = new Point(108, 27);
+            labelOutput.Name = "labelOutput";
+            labelOutput.Size = new Size(286, 86);
+            labelOutput.TabIndex = 0;
+            labelOutput.TextAlign = ContentAlignment.MiddleCenter;
+            labelOutput.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F);
+            button1.Location = new Point(108, 133);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 77);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F);
+            button2.Location = new Point(208, 133);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 77);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F);
+            button3.Location = new Point(308, 133);
+            button3.Name = "button3";
+            button3.Size = new Size(86, 77);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F);
+            button4.Location = new Point(108, 216);
+            button4.Name = "button4";
+            button4.Size = new Size(86, 77);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 18F);
+            button5.Location = new Point(208, 216);
+            button5.Name = "button5";
+            button5.Size = new Size(86, 77);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 18F);
+            button6.Location = new Point(308, 216);
+            button6.Name = "button6";
+            button6.Size = new Size(86, 77);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 18F);
+            button7.Location = new Point(108, 299);
+            button7.Name = "button7";
+            button7.Size = new Size(86, 77);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 18F);
+            button8.Location = new Point(208, 299);
+            button8.Name = "button8";
+            button8.Size = new Size(86, 77);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 18F);
+            button9.Location = new Point(308, 299);
+            button9.Name = "button9";
+            button9.Size = new Size(86, 77);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // buttonPlus
+            // 
+            buttonPlus.Font = new Font("Segoe UI", 18F);
+            buttonPlus.Location = new Point(108, 382);
+            buttonPlus.Name = "buttonPlus";
+            buttonPlus.Size = new Size(86, 77);
+            buttonPlus.TabIndex = 10;
+            buttonPlus.Text = "+";
+            buttonPlus.UseVisualStyleBackColor = true;
+            buttonPlus.Click += buttonPlus_Click;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("Segoe UI", 18F);
+            button0.Location = new Point(208, 382);
+            button0.Name = "button0";
+            button0.Size = new Size(86, 77);
+            button0.TabIndex = 11;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // buttonEqual
+            // 
+            buttonEqual.Font = new Font("Segoe UI", 18F);
+            buttonEqual.Location = new Point(308, 382);
+            buttonEqual.Name = "buttonEqual";
+            buttonEqual.Size = new Size(86, 77);
+            buttonEqual.TabIndex = 12;
+            buttonEqual.Text = "=";
+            buttonEqual.UseVisualStyleBackColor = true;
+            buttonEqual.Click += buttonEqual_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(507, 491);
+            Controls.Add(buttonEqual);
+            Controls.Add(button0);
+            Controls.Add(buttonPlus);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(labelOutput);
+            Name = "Form1";
+            Text = "Kalkulator";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label labelOutput;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button buttonPlus;
+        private Button button0;
+        private Button buttonEqual;
     }
 }
--- TP_MODUL3_103022400117/Form1.cs
+++ TP_MODUL3_103022400117/Form1.cs
@@ -2,9 +2,105 @@ namespace TP_MODUL3_103022400117
 {
     public partial class Form1 : Form
     {
+        int totalJumlah = 0;
+        string angkaSementara = "";
+        string riwayatHitung = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "2";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            if (angkaSementara != "")
+            {
+                totalJumlah = totalJumlah + int.Parse(angkaSementara);
+                labelOutput.Text = riwayatHitung + angkaSementara + " = " + totalJumlah.ToString();
+                angkaSementara = "";
+                riwayatHitung = "";
+                totalJumlah = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "1";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "3";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "4";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "5";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "6";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "7";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "8";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "9";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            angkaSementara = angkaSementara + "0";
+            labelOutput.Text = riwayatHitung + angkaSementara;
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if (angkaSementara != "")
+            {
+                totalJumlah = totalJumlah + int.Parse(angkaSementara);
+                riwayatHitung = riwayatHitung + angkaSementara + " + ";
+                angkaSementara = "";
+                labelOutput.Text = riwayatHitung;
+            }
         }
     }
 }
